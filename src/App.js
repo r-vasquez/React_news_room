44222849d2dd98b882de33eb3d5f62afb600e46a
Rogger Vasquez <rvasque3@gmail.com>
2020-11-27 17:41:45 -0500

    fix for codesandbox

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 
+// For Codesandbox to load icons:
+const styleLink = document.createElement('link');
+styleLink.rel = 'stylesheet';
+styleLink.href = 'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
+document.head.appendChild(styleLink);
+
 function App() {
   return (
     <div className='App'>
